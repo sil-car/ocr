8d5eb64efc132cb29dc19ab7db22d7d37db47d1d
Nate Marti <nate_marti@sil.org>
2022-11-27 08:56:24 +0100

    add verbose option

--- scripts/run-training.sh
+++ scripts/run-training.sh
@@ -3,13 +3,31 @@
 ### Start tesseract training on cloud server with relevant options.
 
 # Set initial variables.
-if [[ -n "$1" ]]; then
-    max_iter="$1"
-else
-    max_iter=100000
-fi
 tess_tr_dir="${HOME}/tesstrain/"
 tess_data="/usr/local/share/tessdata"
+max_iter=100000
+debug_interval=0
+
+help_text="usage: $0 [-v] [-i NUM]"
+while getopts ":hi:v" opt; do
+    case $opt in
+        h) # help text
+            echo "$help_text"
+            exit 0
+            ;;
+        i) # max. iterations
+            max_iter="$OPTARG"
+            ;;
+        v) # verbose output
+            debug_interval=-1
+            ;;
+        *) # invalid option
+            echo "$help_text"
+            exit 1
+            ;;
+    esac
+done
+shift $(($OPTIND - 1))
 
 # Ensure no other virtual environment is active.
 if which deactivate >/dev/null 2>&1; then
@@ -33,7 +51,13 @@ fi
 
 # Start training.
 time_start=$(date +%s)
-make training MODEL_NAME=Latin_afr CORES=2 START_MODEL=Latin TESSDATA="$tess_data" MAX_ITERATIONS="$max_iter"
+make training \
+    MODEL_NAME=Latin_afr \
+    CORES=2 \
+    START_MODEL=Latin \
+    TESSDATA="$tess_data" \
+    MAX_ITERATIONS="$max_iter" \
+    DEBUG_INTERVAL="$debug_interval"
 time_end=$(date +%s)
 duration=$(($time_end - $time_start))
 
