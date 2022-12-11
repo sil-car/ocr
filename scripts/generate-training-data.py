b44d1745478de62ada87177ccfcb8ce02e86fd26
Nate Marti <nate_marti@sil.org>
2022-12-11 20:21:59 +0100

    bump added char rate for a and schwa to 0.005

--- scripts/generate-training-data.py
+++ scripts/generate-training-data.py
@@ -307,8 +307,8 @@ def generate_text_line_weighted_chars(vs, length=40, vowel_wt=1, top_dia_wt=0.5,
     p_vbtdi = 0.10 # of vowels (vowel bottom diacritic)
     p_ctpdi = 0.05 # of consonants (consonant top diacritic)
     # Special probability adjustments.
-    p_a     = 0.002 # to overcome over-recognized alphas
-    p_schwa = 0.002 # to overcome open-o being paired with schwa
+    p_a     = 0.005 # to overcome over-recognized alphas
+    p_schwa = 0.005 # to overcome open-o being paired with schwa
     p_y     = 0.002 # to overcome over-recognized 'v' in place of 'y'
 
     default_options = {
