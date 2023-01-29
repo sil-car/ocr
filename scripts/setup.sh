ae1109ad2565035d8ae4dfa5cc529e0509e9168a
Nate Marti <nate_marti@sil.org>
2023-01-29 17:51:19 +0100

    add output

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,10 +6,11 @@ repo_dir="$(dirname "$script_dir")"
 # Install packaged fonts.
 pkgs=( fonts-lato fonts-noto-core fonts-symbola ttf-mscorefonts-installer )
 for pkg in ${pkgs[@]}; do
-    echo "Installing $pkg"
     if [[ $(dpkg -l | grep $pkg | awk '{print $1}') != 'ii' ]]; then
+        echo "Installing ${pkg}..."
         sudo apt-get install $pkg
     fi
 done
 # Install non-packaged fonts.
+echo "Copying user fonts..."
 cp -fr "${repo_dir}/data/extra-fonts/"* "${HOME}/.local/share/fonts/"
