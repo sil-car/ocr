314ec651a6120a425fc1a3b034bd59125c465d87
Nate Marti <nate_marti@sil.org>
2023-01-29 18:08:53 +0100

    add SIL fonts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,23 @@ script_dir="$(dirname "$0")"
 repo_dir="$(dirname "$script_dir")"
 
 # Install packaged fonts.
-pkgs=( fonts-lato fonts-noto-core fonts-noto-mono fonts-symbola ttf-mscorefonts-installer )
+pkgs=(
+    fonts-lato
+    fonts-noto-core
+    fonts-noto-mono
+    fonts-sil-andika
+    fonts-sil-andika-compact
+    fonts-sil-charis
+    fonts-sil-charis-compact
+    fonts-sil-doulos
+    fonts-sil-doulos-compact
+    fonts-sil-gentium
+    fonts-sil-gentium-basic
+    fonts-sil-gentiumplus
+    fonts-sil-gentiumplus-compact
+    fonts-symbola
+    ttf-mscorefonts-installer
+)
 for pkg in ${pkgs[@]}; do
     if [[ $(dpkg -l | grep $pkg | awk '{print $1}') != 'ii' ]]; then
         echo "Installing ${pkg}..."
