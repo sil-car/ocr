e77315c2b8067d494486c4259fa4cefe0dc4707f
Nate Marti <nate_marti@sil.org>
2023-01-29 17:50:10 +0100

    add output

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,15 +3,10 @@
 script_dir="$(dirname "$0")"
 repo_dir="$(dirname "$script_dir")"
 
-if [[ $(id -u) -ne 0 ]]; then
-    echo "wasta-logos64-setup started as root user."
-    echo "No processing done.  Exiting...."
-    exit 0
-fi
-
 # Install packaged fonts.
 pkgs=( fonts-lato fonts-noto-core fonts-symbola ttf-mscorefonts-installer )
 for pkg in ${pkgs[@]}; do
+    echo "Installing $pkg"
     if [[ $(dpkg -l | grep $pkg | awk '{print $1}') != 'ii' ]]; then
         sudo apt-get install $pkg
     fi
