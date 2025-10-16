2e61a45b5cfc406c34186c8e43df6ab30fb26e52
Nate Marti <nate_marti@sil.org>
2025-10-16 10:13:56 -0500

    update for built tesseract binaries

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -42,38 +42,41 @@ font_pkgs=(
 sudo apt-get install -y "${font_pkgs[@]}"
 
 # Install non-packaged fonts.
-echo "Copying user fonts..."
 if [[ $USER == root ]]; then
     dest_dir=/usr/share/fonts/
 else
     dest_dir="${HOME}/.local/share/fonts/"
 fi
+echo "Copying user fonts..."
 cp -fr "${repo_dir}/data/extra-fonts/"* "$dest_dir"
 
 # Get tesstrain repo.
 if [[ ! -d $HOME/tesstrain ]]; then
+    echo "Getting \"tesstrain\" repository..."
     git clone --depth=1 "https://github.com/tesseract-ocr/tesstrain.git"
 fi
 
 # Get & install tesseract build.
+install_prefix=/usr/local
 tesseract_ver="5.5.1"
 if [[ -z $(which lstmtraining) ]]; then
+    echo "Installing tesseract binaries..."
     rm -rf "${tesseract_ver}"*
     wget "https://github.com/sil-car/tesseract-builds/releases/download/${tesseract_ver}/${tesseract_ver}.zip"
     unzip "${tesseract_ver}.zip"
-    cd "$tesseract_ver"
-    sudo make install
-    sudo make training-install
+    sudo cp -rv "$tesseract_ver"/* ${install_prefix}/
 fi
 
 # eng.traineddata needed to init tesseract
 tessdata_best_repo="https://github.com/tesseract-ocr/tessdata_best"
-if [[ ! -r /usr/local/share/tessdata/eng.traineddata ]]; then
-    wget -P /usr/local/share/tessdata "${tessdata_best_repo}/raw/refs/heads/main/eng.traineddata"
+if [[ ! -r ${install_prefix}/share/tessdata/eng.traineddata ]]; then
+    echo "Installing \"eng.traineddata\" for tesseract initialization..."
+    wget -P ${install_prefix}/share/tessdata "${tessdata_best_repo}/raw/refs/heads/main/eng.traineddata"
 fi
 
 # Get best Latin script traineddata model.
 if [[ ! -r $HOME/tessdata_best/lat.traineddata ]]; then
+    echo "Installing \"lat.traineddata\"..."
     # NOTE: Cloning the full repo requires downloading > 1 GB of data.
     # git clone --depth=1 "https://github.com/tesseract-ocr/tessdata_best.git"
     mkdir -p $HOME/tessdata_best
@@ -82,11 +85,14 @@ fi
 
 # Create & activate venv.
 env_path=$HOME/ocr/env
-python3 -m venv "$env_path"
-source ${env_path}/bin/activate
-if [[ $VIRTUAL_ENV != $env_path ]]; then
-    echo "Error: Failed to activate virtual env."
-    exit 1
+if [[ ! -d $env_path ]]; then
+    echo "Setting up virtual env for Python..."
+    python3 -m venv "$env_path"
+    source ${env_path}/bin/activate
+    if [[ $VIRTUAL_ENV != $env_path ]]; then
+        echo "Error: Failed to activate virtual env."
+        exit 1
+    fi
+    python3 -m pip install -r $HOME/tesstrain/requirements.txt
+    python3 -m pip install -r $HOME/ocr/requirements.txt
 fi
-python3 -m pip install -r $HOME/tesstrain/requirements.txt
-python3 -m pip install -r $HOME/ocr/requirements.txt
