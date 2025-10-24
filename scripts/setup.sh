cc2ba3c8c9e279d8ca32d8820103687fdac50c50
Nate Marti <nate_marti@sil.org>
2025-10-24 08:10:24 -0500

    add output dir for unzip

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -69,7 +69,7 @@ if [[ -z $(which lstmtraining) ]]; then
     echo "Installing tesseract binaries..."
     rm -rf "${tesseract_ver}"*  # just in case...
     wget "https://github.com/sil-car/tesseract-builds/releases/download/${tesseract_ver}/${tesseract_ver}.zip"
-    unzip "${tesseract_ver}.zip"
+    unzip "${tesseract_ver}.zip" -d "$tesseract_ver"
     chmod +x "${tesseract_ver}/bin/"*
     sudo cp --preserve=mode -frv "$tesseract_ver"/* ${install_prefix}/
     rm -rf "${tesseract_ver}"*  # free up disk space
