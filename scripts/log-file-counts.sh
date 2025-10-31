fa435aa967e4602d0035a704ae0235d4e554c8be
Nate Marti <nate_marti@sil.org>
2025-10-31 14:21:28 -0500

    add some space in the output

--- scripts/log-file-counts.sh
+++ scripts/log-file-counts.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 ps -ef | grep -Eo 'python3.*/generate.*$' | head -n1
-echo -n 'PNG:'
+echo -n 'PNG: '
 find ~/ocr/data/training/Latin_afr-ground-truth/ -name '*.png' | wc -l
-echo -n 'BOX:'
+echo -n 'BOX: '
 find ~/ocr/data/training/Latin_afr-ground-truth/ -name '*.box' | wc -l
-echo -n 'LSTMF:'
+echo -n 'LSTMF: '
 find ~/ocr/data/training/Latin_afr-ground-truth/ -name '*.lstmf' | wc -l
