e3dcc6ffebd7a1dd8b9d2f79df37b105027e168b
Nate Marti <nate_marti@sil.org>
2025-10-31 14:51:19 -0500

    use pgrep to match commands

--- scripts/log-file-counts.sh
+++ scripts/log-file-counts.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-ps -ef | grep -Eo 'python3.*/generate.*$' | head -n1
+pgrep -af '.*scripts/generate.*' | cut -d' ' -f3-
+pgrep -af '.*scripts/run-training.*' | cut -d' ' -f3-
 echo -n 'PNG: '
 find ~/ocr/data/training/Latin_afr-ground-truth/ -name '*.png' | wc -l
 echo -n 'BOX: '
