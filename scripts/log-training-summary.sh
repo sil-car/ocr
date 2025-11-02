b091b98fa560e200613e0a5adaf2ce1354209096
Nate Marti <nate_marti@sil.org>
2025-11-02 08:35:25 -0600

    reduce output

--- scripts/log-training-summary.sh
+++ scripts/log-training-summary.sh
@@ -3,4 +3,4 @@
 log_file="$(find "$HOME"/tesstrain/data -name 'Latin_afr_*.log' | sort | tail -n1)"
 grep "New best" "$log_file" | tail -n1
 grep "New worst" "$log_file" | tail -n1
-grep -Ev -e "^Iteration" -e "^File" "$log_file" | tail -n12
+grep -Ev -e "^Iteration" -e "^File" -e "^Unloaded" "$log_file" | tail -n12
