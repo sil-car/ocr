f9a75897522f7ff6c2a2d7296e8ce51c99027b2d
Nate Marti <nate_marti@sil.org>
2025-11-03 15:37:47 -0600

    speed up file counts

--- scripts/log-file-counts.sh
+++ scripts/log-file-counts.sh
@@ -2,9 +2,11 @@
 
 pgrep -af '.*scripts/generate.*' | cut -d' ' -f3-
 pgrep -af '.*scripts/run-training.*' | cut -d' ' -f3-
+tempfile=/tmp/generated-files.txt
+find ~/ocr/data/training/Latin_afr-ground-truth/ -type f > "$tempfile"
 echo -n 'PNG: '
-find ~/ocr/data/training/Latin_afr-ground-truth/ -type f | grep \.png | wc -l
+grep \.png "$tempfile" | wc -l
 echo -n 'BOX: '
-find ~/ocr/data/training/Latin_afr-ground-truth/ -type f | grep \.box | wc -l
+grep \.box "$tempfile" | wc -l
 echo -n 'LSTMF: '
-find ~/ocr/data/training/Latin_afr-ground-truth/ -type f | grep \.lstmf | wc -l
+grep \.lstmf "$tempfile" | wc -l
